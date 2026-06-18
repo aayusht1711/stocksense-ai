4a6fffb4a73b38f055d0fb76c3ee27a5a110a961
Aayush Tripathi <aayushtripathi75@gmail.com>
2026-06-18 22:39:50 +0530

    chore: formatting update in api/main.py

--- api/main.py
+++ api/main.py
@@ -522,3 +522,4 @@ async def websocket_backtest_stream(websocket: WebSocket, ticker: str):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
+
